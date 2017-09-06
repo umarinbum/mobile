338db087163a5b3e09d4a16c4b18fa8dc832a63c
bum <umarin26@hotmail.co.th>
2017-09-06 17:03:52 +0700

    test

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         // this.bindEvents();
-        window.location="http://yanhee.co.th/appointment/";
+        window.location="http://www2.yanhee.co.th/appointment/";
         {
             document.write;
 setTimeout('Redirect()', 100000);
